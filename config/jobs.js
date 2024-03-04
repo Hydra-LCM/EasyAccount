ca014ee9f094f880a4833dcc29a057673e7592ab
Katson Alves <katson.alves@ccc.ufcg.edu.br>
2024-03-03 21:56:28 -0300

    feature/3 - improving environment variables

--- config/jobs.js
+++ config/jobs.js
@@ -1,8 +1,12 @@
 import cron from 'node-cron';
 import ConfirmationAttemptSchema from '../app/models/userConfirmationAttempt.js'
+import dotenv from 'dotenv';
 
+dotenv.config();
 export const configureCronJob = () => {
-    cron.schedule('*/60 * * * *', async () => {
+    const crontime = process.env.CRON_TIME
+    const cronSchedule = `*/${crontime} * * * *`; // Constructing the cron schedule string
+    cron.schedule(cronSchedule, async () => {
         try {
             await ConfirmationAttemptSchema.deleteMany({});
             console.log(`Data in 'ConfirmationAttempt' collection deleted successfully.`);
