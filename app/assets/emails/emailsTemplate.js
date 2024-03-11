12e5b7ad712f04451e05657bda955ae9f5e95169
Katson Alves <katson.alves@ccc.ufcg.edu.br>
2024-03-11 19:59:48 -0300

    refact/emailtemplate

--- app/assets/emails/emailsTemplate.js
+++ app/assets/emails/emailsTemplate.js
@@ -6,14 +6,30 @@ const loadEmailTemplate = (templateFileName) => {
 };
 
 export const confirmationTemplate = (userName, confirmationCode, lang) => {
-    let templateFileName = lang === 'en' ? 'confirmation_email_en.html' : 'confirmation_email_pt.html';
+    let templateFileName;
+    switch (lang) {
+        case 'en':
+            templateFileName = 'confirmation_email_en.html';
+            break;
+        default:
+            templateFileName = 'confirmation_email_pt.html';
+            break;
+    }
     const emailTemplate = loadEmailTemplate(templateFileName);
     
     return emailTemplate.replace('${userName}', userName).replace('${confirmationCode}', confirmationCode);
 };
 
 export const recoveryPassTemplate = (userName, recoveryCode, lang) => {
-    let templateFileName = lang === 'en' ? 'recovery_email_en.html' : 'recovery_email_pt.html';
+    let templateFileName;
+    switch (lang) {
+        case 'en':
+            templateFileName = 'recovery_email_en.html';
+            break;
+        default:
+            templateFileName = 'recovery_email_pt.html';
+            break;
+    }
     const emailTemplate = loadEmailTemplate(templateFileName);
     
     return emailTemplate.replace('${userName}', userName).replace('${recoveryCode}', recoveryCode);
